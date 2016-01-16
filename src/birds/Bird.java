0b1d22ef3f75d1220429e33e2fbc236ed7ee2842
YuliyaRopay <jcolor@ukr.net>
2016-01-16 22:28:47 +0200

    birds

---
+++ src/birds/Bird.java
@@ -0,0 +1,85 @@
+package birds;
+
+import java.util.Comparator;
+
+public class Bird {
+
+    private String name;
+    private double price;
+    private BirdsType type;
+
+    private int count;
+    private int sold;
+
+
+    public Bird(String name) {
+        this(name, BirdsType.NONE, 0, 0);
+    }
+
+    public Bird(String name, double price) {
+        this(name, BirdsType.NONE, 0, price);
+    }
+
+    public Bird(String name, BirdsType type, double price) {
+        this(name, type, 0, price);
+    }
+
+    public Bird(String name, BirdsType type, int count, double price) {
+        this(name, type, count, price,0);
+    }
+
+    public Bird(String name, BirdsType type, int count, double price, int sold) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.count = count;
+        this.sold = sold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public BirdsType getType() {
+        return type;
+    }
+
+    public void setType(BirdsType type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
+    }
+
+    @Override
+    public String toString() {
+        return "["+name+" "+type+",  count:"+count+" price:"+price+" sold:"+sold+"]";
+    }
+
+
+}
---
+++ src/birds/Launcher.java
@@ -0,0 +1,79 @@
+package birds;
+
+import java.util.*;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+        DemoData demoData=new DemoData();
+
+        demoData.addCustomer();
+        //demoData.printCustomers();
+        List<Customer> customerList=demoData.getListCustomers();
+
+        demoData.addBird();
+        //demoData.printBirds();
+        List<Bird> birdsList=demoData.getListBirds();
+
+        demoData.addTransaction();
+        List<Transaction> transactionList=demoData.getTransactionList();
+
+
+        BirdsStore store =new BirdsStore("My first store");
+        store.addListBirds(birdsList);
+        store.addListCustomer(customerList);
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> List of Birds:");
+        store.printAllBirdsList();
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Add new bird - cock:");
+        store.addBird(new Bird("cock", BirdsType.DOMESTIC_BIRD, 40, 50));
+        store.printAllBirdsList();
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Remove bird - duck:");
+        store.removeBird("dUck");
+        store.printAllBirdsList();
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Edit bird count - chick=200:");
+        System.out.println("> Edit bird price - kiwi=550:");
+        store.editBirdCount("Chick", 200);
+        store.editBirdPrice("KIWI", 550);
+        store.printAllBirdsList();
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Price of Birds DESC:");
+        store.printBirdsDescPrice();
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Count of Birds ASC:");
+        store.printBirdsAscCount();
+
+
+        //transaction
+        for(Transaction tr:transactionList){
+            store.soldBirds(tr.getCalendar(),tr.getCustomer(),tr.getBird().getName(),tr.getCount());
+        }
+
+        System.out.println();
+        System.out.println(">>>-----------");
+        System.out.println("> Transaction list:");
+        store.printTransactionByPeriod();
+
+        System.out.println(">>>-----------");
+        System.out.println("> List of Birds after transactions:");
+        store.printAllBirdsList();
+
+
+    }
+
+}
