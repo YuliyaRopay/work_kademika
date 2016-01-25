ed14df2229c1c9d64f0788800bb5373adcaaceea
YuliyaRopay <jcolor@ukr.net>
2016-01-25 22:24:09 +0200

    day10_container

---
+++ src/day10/container/Service.java
@@ -0,0 +1,18 @@
+package day10.container;
+
+public class Service {
+
+    private String name;
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/day10/container/ServiceRepository.java
@@ -0,0 +1,26 @@
+package day10.container;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceRepository<T extends Service> {
+
+    private List<T> services;
+
+    public ServiceRepository() {
+        services=new ArrayList<T>();
+    }
+
+    public void addService(T service){
+        services.add(service);
+    }
+
+    public void removeService(T service){
+        services.remove(service);
+    }
+
+    public List<T> getService(){
+        return services;
+    }
+}
