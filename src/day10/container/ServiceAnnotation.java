2bf26abc3ee86ae9fb54f478cb4d8e88f41f6170
YuliyaRopay <jcolor@ukr.net>
2016-01-30 17:08:26 +0200

    day10_annotation

--- src/day10/container/ServiceAnnotation.java
+++ src/day10/container/ServiceAnnotation.java
@@ -1,5 +1,8 @@
 package day10.container;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ServiceAnnotation {
 }
